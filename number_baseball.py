12ed4f1dbc744a2a655b3dc87ac5dc1f692b3a00
정성원 <37476534+woniwory@users.noreply.github.com>
2022-09-01 00:09:11 +0900

    숫자 야구 게임(20220901)

--- number_baseball.py
+++ number_baseball.py
@@ -4,9 +4,11 @@ def main(player):
     
     while status == 0:
         for p in player:
+            
             print(count, "번째 턴입니다")
             assume = input()
-            for i in range(3):
+            for i in range(3):     
+                
                 sc = 0
                 bc = 0
                 
@@ -16,7 +18,24 @@ def main(player):
                         print(sc)
                     else:
                         bc += 1
-                        
+                                        
              if sc == 3:
                 print("correct")
+                status = 1
+             elif sc > bc:
+                print("strike")
+             elif sc < bc:
+                print("ball")
+             else:
+                print("out")
                 
+                
+player1 = input("player1의 숫자 입력" :).split()
+player2 = input("player2의 숫자 입력" :).split()                    
+                
+player = [player1, player2]
+count = 1
+status = 0
+print("게임 시작 : player1의 선공. player2의 숫자를 맞춰주세요")
+
+main(player)
